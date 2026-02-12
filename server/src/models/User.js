d8eb7c561fa834c52c743cdf31ffb7b24f32f9fc
SudeepKagi <sudeepskagi@gmail.com>
2026-02-12 14:10:36 +0530

    feat: add jwt authentication middleware

---
+++ server/src/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  githubId: String,
+  username: String,
+  avatar: String,
+  accessToken: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
